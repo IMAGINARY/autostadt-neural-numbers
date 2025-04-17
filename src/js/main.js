24d4229f00243d60eb8918407299a6f07d2a5c16
Eric Londaits <eric.londaits@gmail.com>
2025-04-17 12:46:25 -0300

    feat: Add config loader and app scaler.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,36 @@
-console.log('main.js loaded');
+/* eslint-disable no-console */
+import '../sass/default.scss';
+import yaml from 'js-yaml';
+import showFatalError from './helpers-web/show-fatal-error';
+import CfgLoader from './loader/cfg-loader';
+import CfgReaderFetch from './loader/cfg-reader-fetch';
+import AppScaler from './helpers-web/app-scaler';
+
+(async () => {
+  try {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // Accept a settings url param but only if it's made of alphanumeric characters, _ or -, and
+    // has a .yml extension.
+    let settingsFilename = 'settings.yml';
+    const settingsFileUnsafe = urlParams.get('settings');
+    if (urlParams.get('settings')) {
+      if (!urlParams.get('settings').match(/^[a-zA-Z0-9_-]+\.yml$/)) {
+        console.warn('Invalid settings file name. Ignoring. Use only alphanumeric characters, _ or -. and .yml extension.');
+      } else {
+        settingsFilename = settingsFileUnsafe;
+      }
+    }
+
+    const cfgLoader = new CfgLoader(CfgReaderFetch, yaml.load);
+    const config = await cfgLoader.load([
+      'config/app.yml',
+      settingsFilename,
+    ]).catch((err) => {
+      throw new Error(`Error loading configuration: ${err.message}`);
+    });
+  } catch (err) {
+    showFatalError('Fatal error', err);
+    console.error(err);
+  }
+})();
