d42f9275bdab98a6dd162f5256289ac81bf9af6a
Eric Londaits <eric.londaits@gmail.com>
2025-05-02 13:17:29 -0300

    chore: Fix StyleLint errors.

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -68,7 +68,7 @@ module.exports = {
               sassOptions: {
                 loadPaths: ['./node_modules'],
                 quietDeps: true,
-              }
+              },
             },
           },
         ],
